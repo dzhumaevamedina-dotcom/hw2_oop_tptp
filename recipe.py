615cc3def86190b99c397b15ca62c261ce83cc20
dzhumaevamedina-dotcom <dzhumaeva_medina@mail.ru>
2026-06-04 16:10:52 +0300

    подправила вывод как по условию задания

--- recipe.py
+++ recipe.py
@@ -16,7 +16,7 @@ class Ingredient:
         self._quantity = k
 
     def __str__(self):
-        return f"{self.name}:{self.quantity}{self.unit}"
+        return f"{self.name}: {self.quantity} {self.unit}"
 
     def __repr__(self):
         return f"Ingredient('{self.name}',{self.quantity},'{self.unit}')"
