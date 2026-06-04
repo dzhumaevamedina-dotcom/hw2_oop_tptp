263c1310ea4ba175d98da017a3bec877bb83a5bb
dzhumaevamedina-dotcom <dzhumaeva_medina@mail.ru>
2026-06-04 23:10:03 +0300

    второй класс

--- recipe.py
+++ recipe.py
@@ -25,3 +25,41 @@ class Ingredient:
         if not isinstance(obg, Ingredient):
             return False
         return (self.name == obg.name) and (self.unit == obg.unit)
+    
+
+
+class Recipe:
+    def __init__(self,title,ingredients):
+        self.title = title
+        self.ings = ingredients
+
+    def add_ingredient(self,ing):
+        for it in self.ings:
+            if it.name == ing.name and it.unit == ing.unit:
+                it.quantity += ing.quantity
+                return
+        self.ings.append(ing)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        try:
+            return float(ratio) > 0
+        except (TypeError,ValueError):
+            return False
+
+    def scale(self,ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError()
+        arr = []
+        for it in self.ings:
+            arr.append(Ingredient(it.name,it.quantity * ratio,it.unit))
+        return Recipe(self.title,arr)
+
+    def __len__(self):
+        return len(self.ings)
+
+    def __str__(self):
+        s = self.title + ": "
+        for it in self.ings:
+            s += str(it) + ", "
+        return s[:-2]
