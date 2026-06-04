f6b1fab831eaaf753307d20a0b7d80e35076a865
dzhumaevamedina-dotcom <dzhumaeva_medina@mail.ru>
2026-06-05 00:54:29 +0300

    сделала тесты для второго класса

--- test_recipes.py
+++ test_recipes.py
@@ -1,3 +1,4 @@
+#2.1
 import pytest
 from recipe import Ingredient
 
@@ -26,3 +27,53 @@ def test_err():
     x = Ingredient("Масло",250,"мл")
     with pytest.raises(ValueError, match="Количество должно быть положительным"):
         x.quantity = -25
+
+#2.2
+import pytest
+from recipe import Ingredient,Recipe
+
+def test_in():
+    rec = Recipe("Суп",[])
+    assert rec.title == "Суп"
+    assert len(rec) == 0
+
+def test_add1():
+    rec = Recipe("Суп",[])
+    ing1 = Ingredient("Вода",500,"мл")
+    rec.add_ingredient(ing1)
+    assert len(rec) == 1
+    assert rec.ings[0].quantity == 500.0
+
+def test_add2():
+    rec = Recipe("Суп",[])
+    rec.add_ingredient(Ingredient("Вода",500,"мл"))
+    rec.add_ingredient(Ingredient("Вода",200,"мл"))
+    
+    assert len(rec) == 1
+    assert rec.ings[0].quantity == 700.0
+
+def test_scale1():
+    rec = Recipe("Суп",[])
+    rec.add_ingredient(Ingredient("Вода",500,"мл"))
+    rec2 = rec.scale(2)
+    
+    assert rec2.ings[0].quantity == 1000.0
+    assert rec.ings[0].quantity == 500.0
+
+def test_scale2():
+    rec = Recipe("Суп", [])
+    with pytest.raises(ValueError):
+        rec.scale(0)
+
+def test_len():
+    rec = Recipe("Суп", [])
+    rec.add_ingredient(Ingredient("Вода",500,"мл"))
+    rec.add_ingredient(Ingredient("Соль",10,"г"))
+    assert len(rec) == 2
+
+def test_str():
+    rec = Recipe("Суп", [])
+    rec.add_ingredient(Ingredient("Вода",500,"мл"))
+    rec.add_ingredient(Ingredient("Соль",10,"г"))
+    expected = "Суп: Вода: 500.0 мл, Соль: 10.0 г"
+    assert str(rec) == expected
